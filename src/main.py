6001d16dc05e2e07c08dec5fa08ef0f4d139baa5
Samuel Svensson <samme.s90@gmail.com>
2024-05-24 14:54:28 +0200

    Fix direct messages support

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import logging
 
-from discord import Client, Intents, Message
+from discord import Client, Forbidden, Intents, Message
 
 from scripts.config import DISCORD_SECRET, PREFIX, PREFIX_DM
 from scripts.responses import get_responses
@@ -15,21 +15,6 @@ logging.basicConfig(
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
-
-
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        logging.warning("Message was empty because intents were not enabled properly.")
-        return
-
-    if is_dm := user_message[len(PREFIX)] == PREFIX_DM:
-        user_message = user_message[len(PREFIX) + 1:]
-
-    try:
-        response: str = get_responses(user_message)
-        await message.author.send(response) if is_dm else await message.channel.send(response)
-    except Exception as e:
-        logging.error(e)
 
 
 # STARTUP
@@ -48,11 +33,28 @@ async def on_message(message: Message) -> None:
         return
 
     username: str = str(message.author)
-    user_message: str = message.content[len(PREFIX):]
+    user_message: str = message.content.strip()[len(PREFIX):]
     channel: str = str(message.channel)
 
     logging.info(f"<{channel}> {username}: {user_message}")
     await send_message(message, user_message)
+
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        logging.warning("Message was empty because intents were not enabled properly.")
+        return
+
+    if is_dm := user_message.lower().startswith(PREFIX_DM):
+        user_message = user_message[len(PREFIX_DM):]
+    try:
+        response: str = get_responses(user_message)
+        await message.author.send(response) if is_dm else await message.channel.send(response)
+    except Forbidden as e:
+        await message.channel.send(f"{message.author.mention} please enable direct messages from server members.")
+        logging.info(e)
+    except Exception as e:
+        logging.error(e)
 
 
 def main() -> None:
