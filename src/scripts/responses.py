5dc74884e7dfe6792315243b8ea9249a4f3984d3
Samuel Svensson <samme.s90@gmail.com>
2024-05-26 19:55:05 +0200

    Refactor responses

--- src/scripts/responses.py
+++ src/scripts/responses.py
@@ -1,8 +1,8 @@
 from enum import Enum
 from typing import Dict, List
 
-from scripts.ngrok import get_endpoints
 from scripts.config import PREFIX
+from scripts.ngrok import get_endpoints
 
 
 class Commands(Enum):
@@ -27,7 +27,7 @@ def get_response(user_input: str) -> str:
 
 def get_help_response() -> str:
     response: str = ""
-    response += insert_section("books", "Help")
+    response += insert_heading("books", "Help")
     response += insert_newline("Use **!dm**\\{command\\} for direct message.")
     response += insert_heading("tools", "Commands")
     response += insert_newline(f"**{PREFIX}{Commands.HELP.value}**: Show this.")
@@ -38,13 +38,9 @@ def get_help_response() -> str:
 def get_endpoints_response() -> str:
     endpoints = get_endpoints()
     response: str = ""
-    response += insert_section("satellite", "Endpoints/Servers")
+    response += insert_heading("satellite", "Endpoints/Servers")
     response += insert_error("No endpoints found.") if not endpoints else insert_list(endpoints)
     return response.strip()
-
-
-def insert_section(emoji: str, title: str) -> str:
-    return f":{emoji}: **{title}**\n"
 
 
 def insert_heading(emoji: str, title: str) -> str:
